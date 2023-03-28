9a2a2d9e13aea3dea22e6dd1d7ff5a00d58a3e32
Ананьев Александр <ananev-aa@nefteavtomatika.ru>
2023-03-28 08:25:30 +0300

    Update translation

--- omodsim/translations/omodsim_ru.ts
+++ omodsim/translations/omodsim_ru.ts
@@ -1391,7 +1391,6 @@ Length: %2</source>
 <context>
     <name>ScriptControl</name>
     <message>
-        <location filename="../controls/scriptcontrol.ui" line="39"/>
         <source>/***********************************************************************
  This is Open ModSim JavaScript code editor
  © Alexandr Ananev, 2023
@@ -1418,6 +1417,64 @@ Length: %2</source>
  To break script execution use Script.stop() function
 ************************************************************************/
 </source>
+        <translation type="vanished">/***********************************************************************
+ Это редактор Open ModSim JavaScript кода
+ © Alexandr Ananev, 2023
+
+ Встроенные объекты Math, Date and String доступны для использования в коде
+
+ Для чтения и записи регистров используйте объект Server
+
+ var value = Server.readCoil(addr)
+ var value = Server.readDiscrete(addr)		
+ var value = Server.readInput(addr)		
+ var value = Server.readHolding(addr)		
+
+ Server.writeCoil(addr, value)
+ Server.writeDiscrete(addr, value)
+ Server.writeInput(addr, value)	
+ Server.writeHolding(addr, value)	
+
+ Для сохранения значений в периодически запускаемом скрипте используйте объект Storage
+ используйте Storage.setItem(key, value) для сохранения значения
+ используйте Storage.getItem(key) для получения значения 
+ 
+ Для вывода отладочной информации используйте функцию console.log()
+ Для прерывания выполнения скрипта используйте функцию Script.stop()
+************************************************************************/
+</translation>
+    </message>
+    <message>
+        <location filename="../controls/scriptcontrol.ui" line="39"/>
+        <source>/***********************************************************************
+ This is Open ModSim JavaScript code editor
+ © Alexandr Ananev, 2023
+
+ Built-in objects Math, Date and String are available in this script
+
+ To read and write registers use Server object
+
+ var value = Server.readCoil(addr)
+ var value = Server.readDiscrete(addr)		
+ var value = Server.readInput(addr)		
+ var value = Server.readHolding(addr)		
+
+ Server.writeCoil(addr, value)
+ Server.writeDiscrete(addr, value)
+ Server.writeInput(addr, value)	
+ Server.writeHolding(addr, value)	
+
+ To store values through periodically script execution use Storage object
+ use Storage.setItem(key, value) to store value
+ use Storage.getItem(key) to get stored value 
+ 
+ To output debug information use console.log() function
+
+ To break script execution use Script.stop() function
+ To get execution run count of  the script use Script.runCount property. On Script start this property equals 0
+ To get execution script period use Script.period property
+************************************************************************/
+</source>
         <translation>/***********************************************************************
  Это редактор Open ModSim JavaScript кода
  © Alexandr Ananev, 2023
@@ -1442,6 +1499,8 @@ Length: %2</source>
  
  Для вывода отладочной информации используйте функцию console.log()
  Для прерывания выполнения скрипта используйте функцию Script.stop()
+ Для получения информации о количестве выполнений скрипта используйте свойство Script.runCount. При старте скрипта оно равно 0
+ Для получения информации о периоде выполнения скрипта используйте свойство Script.period
 ************************************************************************/
 </translation>
     </message>
