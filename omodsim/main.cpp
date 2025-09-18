c39813c6411386c2a9d6eae33608c50bfe5d0e70
sanny32 <mail@ananev.org>
2025-09-18 19:34:36 +0300

    try console output

--- omodsim/main.cpp
+++ omodsim/main.cpp
@@ -1,7 +1,72 @@
 #include <QApplication>
 #include <QFontDatabase>
+#include <QMessageBox>
 #include "mainwindow.h"
 #include "cmdlineparser.h"
+
+#ifdef Q_OS_WIN
+#include <qt_windows.h>
+#include <io.h>
+#include <fcntl.h>
+#endif
+
+///
+/// \brief g_consoleAttached
+///
+static bool g_consoleAttached = false;
+
+
+///
+/// \brief isConsoleAvailable
+/// \return
+///
+static bool isConsoleAvailable()
+{
+#ifdef Q_OS_WIN
+    if (GetConsoleWindow()){
+        return true;
+    }
+    else if (AttachConsole(ATTACH_PARENT_PROCESS)){
+        g_consoleAttached = true;
+
+        FILE* fDummy;
+        freopen_s(&fDummy, "CONOUT$", "w", stdout);
+        freopen_s(&fDummy, "CONOUT$", "w", stderr);
+
+        setvbuf(stdout, nullptr, _IONBF, 0);
+        setvbuf(stderr, nullptr, _IONBF, 0);
+
+        HANDLE hPipeRead, hPipeWrite;
+        SECURITY_ATTRIBUTES sa{sizeof(sa), nullptr, TRUE};
+        CreatePipe(&hPipeRead, &hPipeWrite, &sa, 0);
+        SetStdHandle(STD_OUTPUT_HANDLE, hPipeWrite);
+        SetStdHandle(STD_ERROR_HANDLE, hPipeWrite);
+
+        return true;
+    }
+    else {
+        STARTUPINFO startupInfo;
+        startupInfo.cb = sizeof(STARTUPINFO);
+        GetStartupInfo(&startupInfo);
+        return (startupInfo.dwFlags & STARTF_USESTDHANDLES);
+    }
+#else
+    return true;
+#endif
+}
+
+///
+/// \brief freeConsoleIfAttached
+///
+static void freeConsoleIfAttached()
+{
+#ifdef Q_OS_WIN
+    if(g_consoleAttached) {
+        FreeConsole();
+        g_consoleAttached = false;
+    }
+ #endif
+}
 
 ///
 /// \brief showVersion
@@ -9,7 +74,13 @@
 static inline void showVersion()
 {
     const auto version = QString("%1\n").arg(APP_VERSION);
-    fputs(qPrintable(version), stdout);
+    if(!isConsoleAvailable()){
+        QMessageBox::information(nullptr, APP_NAME, qPrintable(version));
+    }
+    else {
+        fputs(qPrintable(version), stdout);
+        fflush(stdout);
+    }
 }
 
 ///
@@ -18,7 +89,13 @@ static inline void showVersion()
 ///
 static inline void showHelp(const QString& helpText)
 {
-    fputs(qPrintable(helpText), stdout);
+    if(!isConsoleAvailable()){
+        QMessageBox::information(nullptr, APP_NAME, qPrintable(helpText));
+    }
+    else {
+        fputs(qPrintable(helpText), stdout);
+        fflush(stdout);
+    }
 }
 
 ///
@@ -27,8 +104,16 @@ static inline void showHelp(const QString& helpText)
 ///
 static void showErrorMessage(const QString &message)
 {
-    fputs(qPrintable(message), stderr);
+    if(!isConsoleAvailable()){
+        QMessageBox::critical(nullptr, APP_NAME, qPrintable(message));
+    }
+    else {
+        fputs(qPrintable(message), stderr);
+        fflush(stderr);
+    }
 }
+
+
 
 ///
 /// \brief main
@@ -38,6 +123,8 @@ static void showErrorMessage(const QString &message)
 ///
 int main(int argc, char *argv[])
 {
+    std::atexit(freeConsoleIfAttached);
+
     QApplication a(argc, argv);
     a.setApplicationName(APP_NAME);
     a.setApplicationVersion(APP_VERSION);
