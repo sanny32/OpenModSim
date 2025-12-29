c3fb9028759a741df45a679b8df4ec5473488219
sanny32 <mail@ananev.org>
2025-12-29 16:37:25 +0300

    Developing build script for Linux (RedOS 8, Qt6)

--- build.sh
+++ build.sh
@@ -219,6 +219,11 @@ get_packages() {
             ;;
         rhel-based)
             general_packages="gcc gcc-c++ cmake ninja-build pkgconf-pkg-config xcb-util-cursor-devel"
+            case "$ID"
+                redos)
+                    general_packages="$general_packages libstdc++-static"
+                ;;
+            esac
             ;;
         altlinux)
             general_packages="gcc gcc-c++ cmake ninja-build pkg-config libxcbutil-cursor"
