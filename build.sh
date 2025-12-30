6335414776dcc1aa20841a3785ccb239ae1da2af
sanny32 <mail@ananev.org>
2025-12-30 14:29:55 +0300

    Correcting build script for suse linux

--- build.sh
+++ build.sh
@@ -171,7 +171,7 @@ get_qt5_packages() {
             echo "qt5-base-devel qt5-tools-devel qt5-declarative-devel qt5-serialport-devel qt5-serialbus-devel"
             ;;
         suse-based)
-            echo "libqt5-qtbase-devel libqt5-qttools-devel libqt5-qtdeclarative-devel libqt5-qtserialport-devel libqt5-qtserialbus libqt5-qtserialbus-devel"
+            echo "libqt5-qtbase-devel libqt5-qttools-devel libqt5-qttools-qhelpgenerator libqt5-qtdeclarative-devel libqt5-qtserialport-devel libqt5-qtserialbus libqt5-qtserialbus-devel"
             ;;
     esac
 }
@@ -200,7 +200,7 @@ get_qt6_packages() {
             echo "qt6-base-devel qt6-tools-devel qt6-declarative-devel qt6-serialport-devel qt6-serialbus-devel qt6-5compat-devel"
             ;;
         suse-based)
-            echo "qt6-base-devel qt6-tools-devel qt6-declarative-devel qt6-serialport-devel qt6-serialbus-devel qt6-qt5compat-devel qt6-linguist-devel"
+            echo "qt6-base-devel qt6-tools-devel qt6-declarative-devel qt6-serialport-devel qt6-serialbus-devel qt6-qt5compat-devel qt6-help-devel qt6-linguist-devel"
             ;;
     esac
 }
