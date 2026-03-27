2525372d3649e7e022e0916fe6ac902dcff5c393
sanny32 <mail@ananev.org>
2026-03-27 08:24:52 +0300

    Validators for updated NumericLineEdit

---
+++ src/qintvalidatorex.h
@@ -0,0 +1,21 @@
+#ifndef QINTVALIDATOREX_H
+#define QINTVALIDATOREX_H
+
+#include <QIntValidator>
+
+///
+/// \brief The QIntValidatorEx class
+///
+class QIntValidatorEx final : public QIntValidator
+{
+public:
+    QIntValidatorEx(int bottom, int top, bool allowEmpty, QObject* parent = nullptr);
+
+    State validate(QString& input, int& pos) const override;
+    void fixup(QString& input) const override;
+
+private:
+    bool _allowEmpty;
+};
+
+#endif // QINTVALIDATOREX_H
