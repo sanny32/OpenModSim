2f3d2477812c6421cecb316f4ee659423dce2db8
Alexandr Ananev <mail@ananev.org>
2026-05-08 08:12:50 +0300

    refact: Remove PaletteGuard class and update application style settings

--- src/main.cpp
+++ src/main.cpp
@@ -18,54 +18,6 @@ namespace {
 #endif
 
 ///
-/// \brief The PaletteGuard class
-///
-class PaletteGuard : public QObject {
-public:
-    PaletteGuard(QObject* parent) : QObject(parent) { }
-    bool eventFilter(QObject *obj, QEvent *ev) override {
-        if (ev->type() == QEvent::ApplicationPaletteChange) {
-            QTimer::singleShot(0, [this](){
-                QApplication::setPalette(lightPalette());
-            });
-        }
-        return QObject::eventFilter(obj, ev);
-    }
-
-    static QPalette lightPalette() {
-        const QColor backGround(239, 239, 239);
-        const QColor light = backGround.lighter(150);
-        const QColor mid(backGround.darker(130));
-        const QColor midLight = mid.lighter(110);
-        const QColor base = Qt::white;
-        const QColor disabledBase(backGround);
-        const QColor dark = backGround.darker(150);
-        const QColor darkDisabled = QColor(209, 209, 209).darker(110);
-        const QColor text = Qt::black;
-        const QColor hightlightedText = Qt::white;
-        const QColor disabledText = QColor(190, 190, 190);
-        const QColor button = backGround;
-        const QColor shadow = dark.darker(135);
-        const QColor disabledShadow = shadow.lighter(150);
-        QPalette pal(Qt::black, backGround, light, dark, mid, text, base);
-        pal.setBrush(QPalette::Midlight, midLight);
-        pal.setBrush(QPalette::Button, button);
-        pal.setBrush(QPalette::Shadow, shadow);
-        pal.setBrush(QPalette::HighlightedText, hightlightedText);
-        pal.setBrush(QPalette::Disabled, QPalette::Text, disabledText);
-        pal.setBrush(QPalette::Disabled, QPalette::WindowText, disabledText);
-        pal.setBrush(QPalette::Disabled, QPalette::ButtonText, disabledText);
-        pal.setBrush(QPalette::Disabled, QPalette::Base, disabledBase);
-        pal.setBrush(QPalette::Disabled, QPalette::Dark, darkDisabled);
-        pal.setBrush(QPalette::Disabled, QPalette::Shadow, disabledShadow);
-        pal.setBrush(QPalette::Active, QPalette::Highlight, QColor(48, 140, 198));
-        pal.setBrush(QPalette::Inactive, QPalette::Highlight, QColor(48, 140, 198));
-        pal.setBrush(QPalette::Disabled, QPalette::Highlight, QColor(145, 145, 145));
-        return pal;
-    }
-};
-
-///
 /// \brief isConsoleOutputAvailable
 /// \return
 ///
@@ -146,12 +98,15 @@ int main(int argc, char *argv[])
 
 #ifdef Q_OS_WIN
     a.setStyle("windowsvista");
+#elif defined(Q_OS_MAC)
+    a.setStyle("macos");
 #else
     a.setStyle("Fusion");
-    a.setPalette(PaletteGuard::lightPalette());
-    a.installEventFilter(new PaletteGuard(&a));
 #endif
 
+#if QT_VERSION >= QT_VERSION_CHECK(6, 8, 0)
+    QGuiApplication::styleHints()->setColorScheme(Qt::ColorScheme::Light);
+#endif
     QFontDatabase::addApplicationFont(":/fonts/firacode.ttf");
 
     CmdLineParser parser;
@@ -186,4 +141,3 @@ int main(int argc, char *argv[])
 
     return a.exec();
 }
-
