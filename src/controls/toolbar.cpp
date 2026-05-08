789061a2f467b78145c52a7bd9dbb22ce0ab8a93
Alexandr Ananev <mail@ananev.org>
2026-05-08 08:44:16 +0300

    feat: Add custom ToolBar class with platform font support

---
+++ src/controls/toolbar.cpp
@@ -0,0 +1,68 @@
+#include "toolbar.h"
+
+#include <QActionEvent>
+#include <QApplication>
+#include <QChildEvent>
+#include <QShowEvent>
+#include <QWidget>
+
+///
+/// \brief ToolBar::ToolBar
+/// \param parent
+///
+ToolBar::ToolBar(QWidget* parent)
+    : QToolBar(parent)
+{
+    applyPlatformFont();
+}
+
+///
+/// \brief ToolBar::actionEvent
+/// \param event
+///
+void ToolBar::actionEvent(QActionEvent* event)
+{
+    QToolBar::actionEvent(event);
+    applyPlatformFont();
+}
+
+///
+/// \brief ToolBar::childEvent
+/// \param event
+///
+void ToolBar::childEvent(QChildEvent* event)
+{
+    QToolBar::childEvent(event);
+    if (event->added())
+        applyPlatformFont();
+}
+
+///
+/// \brief ToolBar::showEvent
+/// \param event
+///
+void ToolBar::showEvent(QShowEvent* event)
+{
+    QToolBar::showEvent(event);
+    applyPlatformFont();
+}
+
+///
+/// \brief ToolBar::applyPlatformFont
+///
+void ToolBar::applyPlatformFont()
+{
+#ifdef Q_OS_MAC
+    QFont toolbarFont = QApplication::font();
+    toolbarFont.setPointSizeF(qMax<qreal>(toolbarFont.pointSizeF(), 12.0));
+    setFont(toolbarFont);
+
+    const auto childWidgets = findChildren<QWidget*>();
+    for (auto* widget : childWidgets) {
+        widget->setFont(toolbarFont);
+        widget->setAttribute(Qt::WA_MacSmallSize, false);
+        widget->setAttribute(Qt::WA_MacMiniSize, false);
+        widget->setAttribute(Qt::WA_MacNormalSize, true);
+    }
+#endif
+}
---
+++ src/controls/toolbar.h
@@ -0,0 +1,25 @@
+#ifndef TOOLBAR_H
+#define TOOLBAR_H
+
+#include <QToolBar>
+
+class QActionEvent;
+class QChildEvent;
+class QShowEvent;
+
+class ToolBar : public QToolBar
+{
+    Q_OBJECT
+public:
+    explicit ToolBar(QWidget* parent = nullptr);
+
+protected:
+    void actionEvent(QActionEvent* event) override;
+    void childEvent(QChildEvent* event) override;
+    void showEvent(QShowEvent* event) override;
+
+private:
+    void applyPlatformFont();
+};
+
+#endif // TOOLBAR_H
