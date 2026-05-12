e4e27833f0056eeb7dd25e79d1f556ad0e3f10c4
Alexandr Ananev <mail@ananev.org>
2026-05-12 19:19:22 +0300

    refactor: Remove unused theme-related code and clean up includes in QlementineAppStyle

--- src/styles/qlementineappstyle.cpp
+++ src/styles/qlementineappstyle.cpp
@@ -6,7 +6,6 @@
 #include <QComboBox>
 #include <QDockWidget>
 #include <QPushButton>
-#include <QFontDatabase>
 #include <QHash>
 #include <QLineEdit>
 #include <QLabel>
@@ -31,7 +30,6 @@ using oclero::qlementine::FocusState;
 using oclero::qlementine::MouseState;
 using oclero::qlementine::SelectionState;
 using oclero::qlementine::Status;
-using oclero::qlementine::Theme;
 
 constexpr QRgb kCanvas = 0xffffff;
 constexpr QRgb kChrome = 0xf5f5f5;
@@ -40,25 +38,16 @@ constexpr QRgb kChromePressed = 0xe0e0e0;
 constexpr QRgb kBorder = 0xe3e6ea;
 constexpr QRgb kBorderActive = 0xcdd1d6;
 constexpr QRgb kText = 0x1f2937;
-constexpr QRgb kMutedText = 0x8e8e93;
 constexpr QRgb kDisabledText = 0xb2bac5;
 constexpr QRgb kBlue = 0x007aff;
 constexpr QRgb kBlueHover = 0x1a8aff;
 constexpr QRgb kBluePressed = 0x0062cc;
 constexpr QRgb kBlueDisabled = 0xb3d7ff;
-constexpr QRgb kGreen = 0x34c759;
 
 QColor transparent(QRgb rgb)
 {
     QColor color(rgb);
     color.setAlpha(0);
-    return color;
-}
-
-QColor alpha(QRgb rgb, int value)
-{
-    QColor color(rgb);
-    color.setAlpha(value);
     return color;
 }
 
@@ -78,132 +67,6 @@ const QColor& transparentRef(QRgb rgb)
     if (it == colors.end())
         it = colors.insert(rgb, transparent(rgb));
     return it.value();
-}
-
-Theme makeQlementineAppTheme()
-{
-    Theme theme;
-    theme.meta.name = QStringLiteral("Open ModSim Qlementine");
-    theme.meta.version = QStringLiteral("1.0");
-    theme.meta.author = QStringLiteral("Open ModSim");
-
-    theme.backgroundColorMain1 = QColor(kCanvas);
-    theme.backgroundColorMain2 = QColor(kChrome);
-    theme.backgroundColorMain3 = QColor(kChromeStrong);
-    theme.backgroundColorMain4 = QColor(kChromePressed);
-    theme.backgroundColorMainTransparent = transparent(kCanvas);
-    theme.backgroundColorWorkspace = QColor(kCanvas);
-    theme.backgroundColorTabBar = QColor(kChrome);
-
-    theme.neutralColor = QColor(kChromeStrong);
-    theme.neutralColorHovered = QColor(kChromePressed);
-    theme.neutralColorPressed = QColor(0xd5d5d5);
-    theme.neutralColorDisabled = QColor(0xf2f2f2);
-    theme.neutralColorTransparent = transparent(kChromeStrong);
-
-    theme.primaryColor = QColor(kBlue);
-    theme.primaryColorHovered = QColor(kBlueHover);
-    theme.primaryColorPressed = QColor(kBluePressed);
-    theme.primaryColorDisabled = QColor(kBlueDisabled);
-    theme.primaryColorTransparent = transparent(kBlue);
-
-    theme.primaryColorForeground = QColor(kCanvas);
-    theme.primaryColorForegroundHovered = QColor(kCanvas);
-    theme.primaryColorForegroundPressed = QColor(kCanvas);
-    theme.primaryColorForegroundDisabled = QColor(0xebf4fe);
-    theme.primaryColorForegroundTransparent = transparent(kCanvas);
-
-    theme.secondaryColor = QColor(kText);
-    theme.secondaryColorHovered = QColor(0x111827);
-    theme.secondaryColorPressed = QColor(0x0f172a);
-    theme.secondaryColorDisabled = QColor(kDisabledText);
-    theme.secondaryColorTransparent = transparent(kText);
-
-    theme.secondaryColorForeground = QColor(0x39506b);
-    theme.secondaryColorForegroundHovered = QColor(kBluePressed);
-    theme.secondaryColorForegroundPressed = QColor(0x1f6db8);
-    theme.secondaryColorForegroundDisabled = QColor(kDisabledText);
-    theme.secondaryColorForegroundTransparent = transparent(0x39506b);
-
-    theme.secondaryAlternativeColor = QColor(kMutedText);
-    theme.secondaryAlternativeColorHovered = QColor(kText);
-    theme.secondaryAlternativeColorPressed = QColor(kText);
-    theme.secondaryAlternativeColorDisabled = QColor(kDisabledText);
-    theme.secondaryAlternativeColorTransparent = transparent(kMutedText);
-
-    theme.statusColorSuccess = QColor(kGreen);
-    theme.statusColorSuccessHovered = QColor(0x2fbd54);
-    theme.statusColorSuccessPressed = QColor(0x29a94a);
-    theme.statusColorSuccessDisabled = QColor(0xccefd6);
-    theme.statusColorInfo = QColor(kBlue);
-    theme.statusColorInfoHovered = QColor(kBlueHover);
-    theme.statusColorInfoPressed = QColor(kBluePressed);
-    theme.statusColorInfoDisabled = QColor(kBlueDisabled);
-
-    theme.borderColor = QColor(kBorder);
-    theme.borderColorHovered = QColor(kBorderActive);
-    theme.borderColorPressed = QColor(0xbcc6d2);
-    theme.borderColorDisabled = QColor(0xebeef2);
-    theme.borderColorTransparent = transparent(kBorder);
-
-    theme.semiTransparentColor1 = alpha(kText, 0);
-    theme.semiTransparentColor2 = alpha(kText, 18);
-    theme.semiTransparentColor3 = alpha(kText, 26);
-    theme.semiTransparentColor4 = alpha(kText, 36);
-    theme.semiTransparentColorTransparent = transparent(kText);
-
-    theme.shadowColor1 = alpha(0x000000, 14);
-    theme.shadowColor2 = alpha(0x000000, 20);
-    theme.shadowColor3 = alpha(0x000000, 32);
-    theme.shadowColorTransparent = alpha(0x000000, 0);
-
-    theme.useSystemFonts = true;
-    theme.fontSize = 12;
-    theme.fontSizeMonospace = 12;
-    theme.fontSizeS1 = 11;
-    theme.fontRegular = QFontDatabase::systemFont(QFontDatabase::GeneralFont);
-    theme.fontRegular.setPointSize(theme.fontSize);
-    theme.fontBold = theme.fontRegular;
-    theme.fontBold.setWeight(QFont::DemiBold);
-    theme.fontCaption = theme.fontRegular;
-    theme.fontCaption.setPointSize(theme.fontSizeS1);
-    theme.fontMonospace = QFontDatabase::systemFont(QFontDatabase::FixedFont);
-    theme.fontMonospace.setPointSize(theme.fontSizeMonospace);
-    theme.borderRadius = 6.0;
-    theme.checkBoxBorderRadius = 4.0;
-    theme.borderWidth = 1;
-    theme.controlHeightLarge = 30;
-    theme.controlHeightMedium = 26;
-    theme.controlHeightSmall = 18;
-    theme.controlDefaultWidth = 104;
-    theme.iconSize = QSize(16, 16);
-    theme.iconSizeMedium = QSize(22, 22);
-    theme.iconSizeLarge = QSize(24, 24);
-    theme.iconSizeExtraSmall = QSize(12, 12);
-    theme.spacing = 8;
-    theme.scrollBarThicknessFull = 12;
-    theme.scrollBarThicknessSmall = 6;
-    theme.tabBarPaddingTop = 2;
-    theme.tabBarTabMinWidth = 0;
-    theme.tabBarTabMaxWidth = 0;
-
-    theme.palette.setColor(QPalette::ColorGroup::All, QPalette::Window, theme.backgroundColorMain2);
-    theme.palette.setColor(QPalette::ColorGroup::All, QPalette::Base, theme.backgroundColorMain1);
-    theme.palette.setColor(QPalette::ColorGroup::All, QPalette::AlternateBase, QColor(0xf5f5f5));
-    theme.palette.setColor(QPalette::ColorGroup::All, QPalette::Button, theme.neutralColor);
-    theme.palette.setColor(QPalette::ColorGroup::All, QPalette::Text, theme.secondaryColor);
-    theme.palette.setColor(QPalette::ColorGroup::All, QPalette::WindowText, theme.secondaryColor);
-    theme.palette.setColor(QPalette::ColorGroup::All, QPalette::ButtonText, theme.secondaryColor);
-    theme.palette.setColor(QPalette::ColorGroup::All, QPalette::ToolTipBase, theme.secondaryColor);
-    theme.palette.setColor(QPalette::ColorGroup::All, QPalette::ToolTipText, theme.primaryColorForeground);
-    theme.palette.setColor(QPalette::ColorGroup::All, QPalette::Highlight, theme.primaryColor);
-    theme.palette.setColor(QPalette::ColorGroup::All, QPalette::HighlightedText, theme.primaryColorForeground);
-    theme.palette.setColor(QPalette::ColorGroup::Disabled, QPalette::Text, theme.secondaryColorDisabled);
-    theme.palette.setColor(QPalette::ColorGroup::Disabled, QPalette::WindowText, theme.secondaryColorDisabled);
-    theme.palette.setColor(QPalette::ColorGroup::Disabled, QPalette::ButtonText, theme.secondaryColorDisabled);
-    theme.palette.setColor(QPalette::ColorGroup::Disabled, QPalette::ToolTipText, theme.primaryColorForegroundDisabled);
-
-    return theme;
 }
 
 QMargins tabShapePadding(const QStyleOptionTab& option, int spacing)
@@ -231,7 +94,6 @@ QMargins tabShapePadding(const QStyleOptionTab& option, int spacing)
 QlementineAppStyle::QlementineAppStyle(QObject* parent)
     : QlementineStyle(parent)
 {
-    setTheme(makeQlementineAppTheme());
     setAutoIconColor(oclero::qlementine::AutoIconColor::ForegroundColor);
 
     oclero::qlementine::icons::initializeIconTheme();
